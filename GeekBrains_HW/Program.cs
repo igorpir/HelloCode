6e30a10dbc05287db9f9e8585300b0eb1571a906
Ivan Talanov <8ivanmir@gmail.com>
2023-05-28 15:37:22 +0300

    GeekBrains_HW_1

---
+++ GeekBrains_HW/Program.cs
@@ -0,0 +1,20 @@
+﻿// дз 1 задача 1;
+Console.Clear();
+int max = 0;
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+ {
+ max = a;
+ }
+else 
+{max = b;
+}
+int result = max;
+
+Console.WriteLine(result);
+
